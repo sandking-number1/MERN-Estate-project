35dad209ef2260286314e8f85dac9483149725ad
sandking-number1 <Alfred.J9412@gmail.com>
2024-02-01 21:03:35 +0100

    Created header section

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signin from './pages/Signin'
-import SignUp from './pages/signUp'
+import SignUp from './pages/Sign'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/header'
 
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <Header />
   <Routes>
     <Route path='/' element={<Home /> } />
     <Route path='/sign-in' element={<Signin /> } />
-    <Route path='/sign-up' element={<signUp /> } />
+    <Route path='/sign-up' element={<SignUp /> } />
     <Route path='/about' element={<About /> } />
     <Route path='/profile' element={<Profile /> } />
   </Routes>
   </BrowserRouter>
+  )
 }
