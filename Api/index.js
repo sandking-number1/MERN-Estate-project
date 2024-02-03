b90622e7c5110e1dc3d5d60256e8a5113e6d11d2
sandking-number1 <Alfred.J9412@gmail.com>
2024-02-03 21:17:58 +0100

    connected to database

--- Api/index.js
+++ Api/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
+  .then(() => {console.log('connected to MONGO');})
+  .catch((err) => {console.log(err);})
 
 const app = express();
 
