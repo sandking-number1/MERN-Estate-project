7333d716f6bdf225eac2f7eca972e23652fe2de8
sandking-number1 <Alfred.J9412@gmail.com>
2024-03-20 16:04:08 +0100

    create profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+//import React from 'react'
+import { useSelector } from "react-redux"
 
 const Profile = () => {
+
+  const {currentUser} = useSelector((state) => state.user)
+
   return (
-    <div>Profile</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img src={currentUser.avatar} alt="profile-pic"  className="rounded-full h-24 w-24 object-cover cursor-pointer self-center m-2"/>
+
+        <input type="text" className="border p-3 rounded-lg" placeholder="username" id="username"/>
+        <input type="email" className="border p-3 rounded-lg" placeholder="email" id="email" />
+        <input type="text" className="border p-3 rounded-lg" placeholder="password" id="password"/>
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">Update</button>
+
+        <div className=" flex justify-between mt-5">
+          <span className="text-red-700 cursor-pointer">Delete Account</span>
+          <span className="text-red-700 cursor-pointer">sign out</span>
+        </div>
+      </form>
+    </div>
   )
 }
 
