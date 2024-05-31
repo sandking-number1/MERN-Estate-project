8c57a6aa20254590f82048c63d7a0e761fcd0322
sandking-number1 <Alfred.J9412@gmail.com>
2024-05-31 03:07:19 +0100

    jwt verify middleware

--- Api/utilis/verifyUser.js
+++ Api/utilis/verifyUser.js
@@ -1,15 +1,18 @@
-import jwt from 'jsonwebtoken';
-import { errorHandler } from './error.js';
+const  jwt = require('jsonwebtoken');
+const errorHandler = require('./error');
 
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token; // get the token in the cookie
 
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {  // verify the token
     if (err) return next(errorHandler(403, 'Forbidden'));
 
-    req.user = user;
+    req.user = user;  // save the user object inn the request
     next(); // go the updateUser
   });
 };
+
+module.exports = verifyToken
+
